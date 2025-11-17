93a986149f7eef61715771a168e4e237cad0e3cc
gbzhang23 <grayxin.zhang@gmail.com>
2025-11-17 16:28:44 -0600

    Grace Zhang (Nov 17) - Finished documents page, has clickable assignments that lead to a full-screen version of notespanel.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
         <Navbar
             bg="white"
             className="shadow-sm mb-4"
-            style={{ padding: "0.75rem 1rem", margin: 10 }}
+            style={{ padding: "0.75rem 1rem", margin: 12 }}
         >
             <Container className="d-flex justify-content-center">
                 <Nav className="d-flex gap-4">
