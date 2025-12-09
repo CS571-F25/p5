b13fc9433b92f093cd0f71f8797e5488cbb9f1af
gbzhang23 <grayxin.zhang@gmail.com>
2025-12-09 13:39:53 -0600

    Grace Zhang (Dec 9) - Added calendar and others to stats page

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -4,7 +4,9 @@ export default function StatCard({ title, value }) {
     return (
         <Card className="text-center p-3 rounded-3 shadow-sm h-100 d-flex flex-column justify-content-center">
             <h5 className="mb-2">{title}</h5>
-            <h2 className="fw-bold">{value}</h2>
+            <div className="fw-bold" style={{ fontSize: "1.75rem" }}>
+                {value}
+            </div>
         </Card>
     );
 }
